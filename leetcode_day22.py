62503d3487724a33ca9fb3d5aa9a689834f9330c
UETk70_Phạm Đại <25020102@vnu.edu.vn>
2026-03-05 21:45:33 +0700

    Add files via upload

---
+++ leetcode_day22.py
@@ -0,0 +1,25 @@
+#problem 70
+from math import *
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cnt = 0
+
+        for i in range(n//2 + 1):
+            a = i
+            b = n - 2*a
+            cnt += comb(a + b, b)
+        
+        return cnt
+#problem 119
+from math import *
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        cnt = []
+        
+        for i in range(rowIndex + 1):
+            cnt.append(comb(rowIndex,i))
+        
+        return cnt
+
+
+        
